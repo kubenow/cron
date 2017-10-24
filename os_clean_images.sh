ecf7ad30d0352f6bb7d3e320107aab5ac247b621
carmat88 <matteo.carone@farmbio.uu.se>
2017-10-24 17:51:33 +0200

    Fixing pip related errors for Opestack script

--- os_clean_images.sh
+++ os_clean_images.sh
@@ -7,7 +7,7 @@ set -e
 sudo sed -i /etc/hosts -e "s/^127.0.0.1 localhost$/127.0.0.1 localhost $(hostname)/"
 
 # Installing necessary tool for the script: python-glanceclient
-pip install python-glanceclient
+sudo pip install --upgrade python-glanceclient
 
 echo -e "----------------------------------\n   $(date)   \n----------------------------------"
 del_date=$(date +"%Y-%m-%d" --date="1 days ago")
